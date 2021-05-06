ecf410c0706b2c6071e801b685a2661c6a57434c
Alex <cernikov@mail.ru>
2021-05-06 18:33:53 +0300

    home

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,25 @@
 
-$(document).ready(function () {
+// $(document).ready(function () {
 
-  $('.header-navbar__hamburger').on('click', function () {
+//   $('.header-navbar__hamburger').on('click', function () {
+//     $(this).toggleClass('active');
+//     $('.header-navbar__list').toggleClass('active');
+//   });
+
+// });
+
+
+
+// $(document).ready(function(){
+//   $('.header-navbar__btn').on('click', function(){
+//     $(this).toggleClass('active');
+//     $('.header-navbar__list').stop(true, true).slideToggle(500)
+//   });
+// });
+
+$(document).ready(function(){
+  $('.header-navbar__btn').click(function(){
     $(this).toggleClass('active');
-    $('.header-navbar__list').toggleClass('active');
+    $('.header-navbar__list').slideToggle(500)
   });
-
 });
